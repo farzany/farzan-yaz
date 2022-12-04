27dcb30f6633e130b9bb918079ae8e51aee68548
Farzan Yazdanjou <farzan.yazdanjou@coconutsoftware.com>
2022-12-03 22:33:23 -0500

    Redesigning the home page - added latest post card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,31 @@
     <div class="">
         <div id="about" class="pt-7">
             <p class="dg text-md font-work text-center">Nov 14 2022</p>
-            <h1 class="text-center text-3xl font-semibold font-work p-3 mb-5">
-                Hello! I'm Farzan Yazdanjou, a Computer Science student at McMaster University, currently interning at Coconut Software as a Software Developer! 🥥🌴
+            <h1 class="text-center text-3xl font-semibold font-work p-3 mb-2">
+                Hello! I'm Farzan Yazdanjou, a Computer Science student at McMaster University.
             </h1>
-            <div class="mb-10 w-full h-64 bg-[url('/../storage/app/public/cover-photo.jpg')] bg-cover bg-center rounded-2xl"></div>
-            <h2 class="text-xl font-semibold border-l-[6px] border-sky-600 bg-sky-50 py-1 pl-2 my-5">About Me</h2>
+            <p class="dg text-xl font-work text-center mb-5">I'm currently interning at Coconut Software as a Software Developer! 🥥🌴</p>
+            <div class="mb-10 w-full h-96 bg-[url('/../storage/app/public/cover-photo.jpg')] bg-cover bg-center rounded-2xl"></div>
+            <div>
+                <p class="body mb-3">
+                    I'm a third year student at McMaster University studying Computer Science. As a full-stack programmer, I am passionate about web-app development! For more information about my academic career, please visit my LinkedIn profile!
+                </p>
+                <p class="body mb-5">
+                    I have 3000 hours on CSGO, which basically makes me a professional gamer. I love biking through trails and the city and photographing the scenary. I've watched the Iron Man trilogy countless times. I'm always listening to music - my favorite song is Mutual Butterflies by Ryan Trey. I've lost my fair share of money gambling on Cryptocurrency. As one does, I of course also love memes.
+                </p>
+            </div>
+            <div>
+                <h2 class="text-xl font-work mb-2">Check out what I'm listening to ✨</h2>
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4wNpNCgwh82TcUzK3CT5Sn?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="mt-14">
+                <h2 class="font-work text-3xl text-center">Latest Blog Post</h2>
+                <x-post-card :post="$post" />
+            </div>
+            {{-- <h2 class="text-sky-700 text-2xl font-work mb-2">Projects</h2>
             <p class="body">
-                I'm a third year student at McMaster University studying Computer Science. I did Software Engineering for my first two years, but realized I prefer the CS curriculum, instead. I am currently passionate about discovering and automating time-consuming, repetitive tasks. For more information about my academic career, please visit my LinkedIn profile!
-                <br><br>
-                In under 100 words: I have 3000 hours on CSGO, which basically makes me a professional gamer. I love biking through trails and the city. I find photography relaxing and post my favorite shots on my Instagram page. I'm a big fan of the MCU with Iron Man as my favorite! I'm always listening to music - my favorite album is Get Rich or Die Trying by 50 Cent. I'm a Cryptocurrency connoisseur. I'm fairly skilled with video and photo editing which has come from my brief Minecraft YouTube career back in highschool. As one does, I of course also love memes.
-            </p>
-            <h2 class="text-xl font-semibold border-l-[6px] border-sky-600 bg-sky-50 py-1 pl-2 my-5">Projects</h2>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, atque perferendis. Aut, dolorum praesentium. Dolores magnam commodi quas! Amet hic quaerat suscipit sunt blanditiis aut cumque eos quisquam aspernatur quis?
+            </p> --}}
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'post' => Post::latest('created_at')->first(),
+    ]);
 });
 
 Route::get('posts', function () {
