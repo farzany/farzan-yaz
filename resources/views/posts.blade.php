48726283e3f0f6920b58da7f870595a115a469d4
Farzan Yazdanjou <farzan.yazdanjou@coconutsoftware.com>
2022-12-03 22:32:34 -0500

    Moved the post card content into a separate component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -20,26 +20,7 @@
         </div>
     </div>
     @foreach ($posts as $post)
-        <a href="/posts/{{ $post->slug }}" class="trasition ease-in-out hover:-translate-y-1 hover:scale-110">
-            <article class="post my-7 p-5 rounded-2xl flex gap-5 justify-between bg-sky-50">
-                <div class="flex justify-center flex-col w-2/3">
-                    <p class="pb-1 dg text-sm font-work">
-                        {{ date('M jS Y', strtotime($post->created_at)); }}
-                        <span class="px-1">·</span>
-                        {{ $post->category->name }}
-                        <span class="px-1">·</span>
-                        {{ $post->getReadDuration() }} min read
-                    </p>
-                    <h2 class="font-bold text-[1.4rem]">
-                            {{ $post->title }}
-                    </h2>
-                    <div class="pt-1 text-base line-clamp-2">
-                        {{ $post->excerpt }}
-                    </div>
-                </div>
-                <div style="background-image: url('/storage/{{ $post->image }}')" class="thumbnail shrink-0 w-48 h-27 bg-center rounded-xl"></div>
-            </article>
-        </a>
+        <x-post-card :post="$post" />
     @endforeach
     <div class="pagination">
         {{ $posts->links() }}
