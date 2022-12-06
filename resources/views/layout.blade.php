c4ec8efd8f42a7cc0c3242230a3e5d7dea0a44bf
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2022-12-06 02:22:30 -0500

    Added Favicons

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,14 @@
     <meta charset="UTF-8">
     <meta name="description" content="TODO"> <!-- TODO -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="storage/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="storage/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="storage/favicon/favicon-16x16.png">
+    <link rel="manifest" href="storage/favicon/site.webmanifest">
+    <link rel="mask-icon" href="storage/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <!-- Scripts and Symbols -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script async src="https://static.addtoany.com/menu/page.js"></script>
