5446cb5fcd6eb1c9c4b15c5a41cd19a8127e76d3
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2022-12-06 01:20:08 -0500

    Making the snackbar notifications responsive

--- resources/views/components/snack-bar.blade.php
+++ resources/views/components/snack-bar.blade.php
@@ -3,6 +3,7 @@
         x-data="{ show: true }"
         x-init="setTimeout(() => show = false, 5000)"
         x-show="show"
+        id="snackbar"
         class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-10 left-10 z-50 shadow-md"
     >
         <p class="text-white font-work">{{ session('success') }}</p>
@@ -14,6 +15,7 @@
         x-data="{ show: true }"
         x-init="setTimeout(() => show = false, 5000)"
         x-show="show"
+        id="snackbar"
         class="fixed bg-red-500 text-white py-2 px-4 rounded-xl bottom-10 left-10 z-50 shadow-md"
     >
         <p class="text-white font-work">{{ session('error') }}</p>
