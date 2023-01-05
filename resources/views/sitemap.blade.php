0503f6d50bf7e5f730530bcc4f836ea579a6ff77
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-01-04 20:03:48 -0500

    Improved sitemap to include home, posts, resume, projects

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,25 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://farzanyaz.com/</loc>
+        <lastmod>2023-01-05T01:00:54+00:00</lastmod>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>https://farzanyaz.com/posts</loc>
+        <lastmod>2023-01-05T01:00:54+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://farzanyaz.com/resume</loc>
+        <lastmod>2023-01-05T01:00:54+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://farzanyaz.com/projects</loc>
+        <lastmod>2023-01-05T01:00:54+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
     @foreach ($posts as $post)
         <url>
             <loc>{{ url('/') }}/posts/{{ $post->slug }}</loc>
