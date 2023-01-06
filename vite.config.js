12ff8dc522a2928e82ee2cc2067bf2aba7eba269
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-01-06 17:05:31 -0500

    Integrating laravel ui react

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,10 +8,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/prism.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/prism.js'
             ],
             refresh: true,
         }),
+        react({ fastRefresh: false }),
     ],
 });
