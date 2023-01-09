9a5ac0a9d09c12919fd21cf00b72a1cdd00c5b1e
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-01-09 00:53:27 -0500

    Added CSRF and default MUI google font links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <meta name="description" content="{{ $metaDescription ?? '' }}">
     <meta property="og:image" content="{{ "../storage/$metaImage" }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/storage/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/storage/favicon/favicon-32x32.png">
@@ -28,6 +29,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,400&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300;1,400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
 </head>
 
 <title> {{ $metaTitle ?? 'Farzan Yazdanjou' }} </title>
