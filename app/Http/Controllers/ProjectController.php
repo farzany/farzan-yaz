ea65b3d4352bbf3e8c27d718a8a94905084f4839
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-03-10 13:03:50 -0500

    Changed how sorting is done on projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
     public function all()
     {
         return view('projects', [
-            'projects'  => Project::latest('created_at')->get(),
+            'projects'  => Project::latest('id')->get(),
             'metaTitle' => 'Projects - Farzan Yazdanjou',
             'metaDescription' => "Learn about the various software development projects I've tackled as I strive to improve my skills and share my knowledge. Check out my progress and see how I'm constantly growing as a developer.",
             'metaImage' => 'cover-photo.jpg',
