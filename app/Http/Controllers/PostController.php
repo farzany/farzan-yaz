3d14f5325de7cd3775cdd9d8078455f29947620c
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-05-19 23:03:03 -0400

    Let's not cache them for now lol

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,9 +16,7 @@ class PostController extends Controller
     public function all()
     {
         return view('posts', [
-            'posts' => cache()->remember("posts", now()->addDays(5), function () {
-                return Post::latest('created_at')->paginate();
-            }),
+            'posts' => Post::latest('created_at')->paginate(),
             'categories' => Category::all(),
             'metaTitle' => 'Posts - Farzan Yazdanjou',
             'metaDescription' => 'Hey! I post short articles about tech, programming tutorials, career discussions, and more. At least one a week - with an accompanying YouTube video.',
@@ -34,9 +32,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('post', [
-            'post' => cache()->remember("posts.{$post->slug}", now()->addDay(), function () use ($post) {
-                return $post;
-            }),
+            'post' => $post,
             'metaTitle' => "$post->title - Farzan Yazdanjou",
             'metaDescription' => $post->excerpt,
             'metaImage' => "posts/$post->image",
